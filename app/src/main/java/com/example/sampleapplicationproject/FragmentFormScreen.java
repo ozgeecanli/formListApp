8b83bb714877164dc39b9a45cfac1c5785a405e0
Özge Canlı <ozgeecanli@gmail.com>
2021-11-12 18:57:52 +0300

    Change names according to template on fragment_form_screen.

--- app/src/main/java/com/example/sampleapplicationproject/FragmentFormScreen.java
+++ app/src/main/java/com/example/sampleapplicationproject/FragmentFormScreen.java
@@ -41,11 +41,10 @@ public class FragmentFormScreen extends Fragment {
         View setContentView = inflater.inflate(R.layout.fragment_form_screen, container,
                 false);
         // initializing our variables.
-        courseNameEdt = setContentView.findViewById(R.id.idEdtCourseName);
-        courseDescEdt = setContentView.findViewById(R.id.idEdtCourseDescription);
-        addBtn = setContentView.findViewById(R.id.idBtnAdd);
-        saveBtn = setContentView.findViewById(R.id.idBtnSave);
-        courseRV = setContentView.findViewById(R.id.idRVCourses);
+        courseNameEdt = setContentView.findViewById(R.id.editTextFormName);
+        courseDescEdt = setContentView.findViewById(R.id.editTextFormSurname);
+        addBtn = setContentView.findViewById(R.id.buttonFormSave);
+        courseRV = setContentView.findViewById(R.id.recyclerViewNameSurname);
 
         // calling method to load data
         // from shared prefs.
@@ -96,42 +95,52 @@ public class FragmentFormScreen extends Fragment {
         courseRV.setAdapter(adapter);
     }
 
-   private void loadSaveData() {
-       // method to load arraylist from shared prefs
-       // initializing our shared prefs with name as
-       // shared preferences.
-       SharedPreferences sharedPreferences = this.getActivity().
-               getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
-       // creating a variable for gson.
-       Gson gson = new Gson();
-       // below line is to get to string present from our
-       // shared prefs if not present setting it as null.
-       String json = sharedPreferences.getString("courses", null);
-       // below line is to get the type of our array list.
-       Type type = new TypeToken<ArrayList<CourseModal>>() {
-       }.getType();
-       // in below line we are getting data from gson
-       // and saving it to our array list
-       courseModalArrayList = gson.fromJson(json, type);
-       // checking below if the array list is empty or not
-       if (courseModalArrayList == null) {
-           // if the array list is empty
-           // creating a new array list.
-           courseModalArrayList = new ArrayList<>();
-       }
+    private void loadSaveData() {
+        // method to load arraylist from shared prefs
+        // initializing our shared prefs with name as
+        // shared preferences.
+        SharedPreferences sharedPreferences = this.getActivity().
+                getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+
+        // creating a variable for gson.
+        Gson gson = new Gson();
+
+        // below line is to get to string present from our
+        // shared prefs if not present setting it as null.
+        String json = sharedPreferences.getString("courses", null);
+
+        // below line is to get the type of our array list.
+        Type type = new TypeToken<ArrayList<CourseModal>>() {
+        }.getType();
+
+        // in below line we are getting data from gson
+        // and saving it to our array list
+        courseModalArrayList = gson.fromJson(json, type);
+
+        // checking below if the array list is empty or not
+        if (courseModalArrayList == null) {
+            // if the array list is empty
+            // creating a new array list.
+            courseModalArrayList = new ArrayList<>();
+        }
         // creating a variable for editor to
         // store data in shared preferences.
         SharedPreferences.Editor editor = sharedPreferences.edit();
+
         // creating a new variable for gson.
         Gson gson2 = new Gson();
+
         // getting data from gson and storing it in a string.
         String json2 = gson2.toJson(courseModalArrayList);
+
         // below line is to save data in shared
         // prefs in the form of string.
         editor.putString("courses", json2);
+
         // below line is to apply changes
         // and save data in shared prefs.
         editor.apply();
+
         // after saving data we are displaying a toast message.
         Toast.makeText(getActivity(), "Saved Array List to Shared preferences. ", Toast.LENGTH_SHORT).show();
     }
