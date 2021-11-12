d2aa961feeeea9900a70b4777d45dad75bc9e0bf
Özge Canlı <ozgeecanli@gmail.com>
2021-11-12 17:50:15 +0300

    Save ArrayList to SharedPreferences on FragmentFormScreen.

--- app/src/main/java/com/example/sampleapplicationproject/FragmentFormScreen.java
+++ app/src/main/java/com/example/sampleapplicationproject/FragmentFormScreen.java
@@ -1,5 +1,6 @@
 package com.example.sampleapplicationproject;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,147 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.SharedPreferences;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class FragmentFormScreen extends Fragment {
+
+    // creating variables for our ui components.
+    private EditText courseNameEdt, courseDescEdt;
+    private Button addBtn, saveBtn;
+    private RecyclerView courseRV;
+
+    // variable for our adapter class and array list
+    private CourseAdapter adapter;
+    private ArrayList<CourseModal> courseModalArrayList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form_screen, container, false);
+        //return inflater.inflate(R.layout.fragment_form_screen, container, false);
+        View setContentView = inflater.inflate(R.layout.fragment_form_screen, container,
+                false);
+        // initializing our variables.
+        courseNameEdt = setContentView.findViewById(R.id.idEdtCourseName);
+        courseDescEdt = setContentView.findViewById(R.id.idEdtCourseDescription);
+        addBtn = setContentView.findViewById(R.id.idBtnAdd);
+        saveBtn = setContentView.findViewById(R.id.idBtnSave);
+        courseRV = setContentView.findViewById(R.id.idRVCourses);
+
+        // calling method to load data
+        // from shared prefs.
+        loadData();
+
+        // calling method to build
+        // recycler view.
+        buildRecyclerView();
+
+        // on click listener for adding data to array list.
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // below line is use to add data to array list.
+                courseModalArrayList.add(new CourseModal(courseNameEdt.getText().toString(), courseDescEdt.getText().toString()));
+                // notifying adapter when new data added.
+                adapter.notifyItemInserted(courseModalArrayList.size());
+            }
+        });
+        // on click listener for saving data in shared preferences.
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling method to save data in shared prefs.
+                saveData();
+            }
+        });
+        return setContentView;
+    }
+
+    private void buildRecyclerView() {
+
+        // initializing our adapter class.
+        adapter = new CourseAdapter(courseModalArrayList, getContext());
+
+        // adding layout manager to our recycler view.
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        //courseRV.setHasFixedSize(true);
+
+        // setting layout manager to our recycler view.
+        courseRV.setLayoutManager(manager);
+
+        // setting adapter to our recycler view.
+        courseRV.setAdapter(adapter);
+    }
+
+    private void loadData() {
+        // method to load arraylist from shared prefs
+        // initializing our shared prefs with name as
+        // shared preferences.
+        SharedPreferences sharedPreferences = this.getActivity().
+                getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+
+        // creating a variable for gson.
+        Gson gson = new Gson();
+
+        // below line is to get to string present from our
+        // shared prefs if not present setting it as null.
+        String json = sharedPreferences.getString("courses", null);
+
+        // below line is to get the type of our array list.
+        Type type = new TypeToken<ArrayList<CourseModal>>() {
+        }.getType();
+
+        // in below line we are getting data from gson
+        // and saving it to our array list
+        courseModalArrayList = gson.fromJson(json, type);
+
+        // checking below if the array list is empty or not
+        if (courseModalArrayList == null) {
+            // if the array list is empty
+            // creating a new array list.
+            courseModalArrayList = new ArrayList<>();
+        }
+    }
+
+    private void saveData() {
+        // method for saving the data in array list.
+        // creating a variable for storing data in
+        // shared preferences.
+        SharedPreferences sharedPreferences = this.getActivity().
+                getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+
+        // creating a variable for editor to
+        // store data in shared preferences.
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // creating a new variable for gson.
+        Gson gson = new Gson();
+
+        // getting data from gson and storing it in a string.
+        String json = gson.toJson(courseModalArrayList);
+
+        // below line is to save data in shared
+        // prefs in the form of string.
+        editor.putString("courses", json);
+
+        // below line is to apply changes
+        // and save data in shared prefs.
+        editor.apply();
+
+        // after saving data we are displaying a toast message.
+        Toast.makeText(getActivity(), "Saved Array List to Shared preferences. ", Toast.LENGTH_SHORT).show();
     }
 }
