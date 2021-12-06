18b0e161d5c9b9436ad62118fa97440000110480
Özge Canlı <ozgeecanli@gmail.com>
2021-12-07 00:14:04 +0300

    Create interface class which is OnSelectAccountListener.

---
+++ app/src/main/java/com/example/sampleapplicationproject/OnSelectAccountListener.java
@@ -0,0 +1,9 @@
+package com.example.sampleapplicationproject;
+
+import com.example.sampleapplicationproject.models.CustomAccountModel;
+
+public interface OnSelectAccountListener {
+
+    void onAccountSelected(CustomAccountModel accountModel);
+
+}
