a6a5574169cfec6a42f3cc30e72223308aecfbd2
Özge Canlı <ozgeecanli@gmail.com>
2022-01-17 18:38:45 +0300

    Add button for language selection.

--- app/src/main/java/com/example/sampleapplicationproject/ui/FragmentHome.java
+++ app/src/main/java/com/example/sampleapplicationproject/ui/FragmentHome.java
@@ -1,21 +1,64 @@
 package com.example.sampleapplicationproject.ui;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.sampleapplicationproject.LocaleHelper;
 import com.example.sampleapplicationproject.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FragmentHome extends BaseFragment {
+
+    @BindView(R.id.textViewLanguage)
+    TextView messageView;
+    @BindView(R.id.buttonTurkish)
+    Button buttonTurkish;
+    @BindView(R.id.buttonEnglish)
+    Button buttonEnglish;
+
+    Context context;
+    Resources resources;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        buttonEnglish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context = LocaleHelper.setLocale(getContext(), "en");
+                resources = context.getResources();
+                messageView.setText(resources.getString(R.string.language));
+            }
+        });
+
+        buttonTurkish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context = LocaleHelper.setLocale(getContext(), "hi");
+                resources = context.getResources();
+                messageView.setText(resources.getString(R.string.language));
+            }
+        });
     }
 }
